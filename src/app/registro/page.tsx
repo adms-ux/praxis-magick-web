59131d039fcb9f46eeab53121d6cd35cd057de9f
adms-ux <bryanmendezmota0@gmail.com>
2026-09-16 17:27:36 -0600

    Actualizar page.tsx

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useToast } from "../../Context/ToastContext";
+import { useToast } from "../Context/ToastContext";
 
 export default function PortalRegistro() {
   const router = useRouter();
